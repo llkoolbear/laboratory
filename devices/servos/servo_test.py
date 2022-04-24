4b89b032b90e92d5a93b6c4d0ce6f2f067ea25ab
Bear Bissen <bearbissen@gmail.com>
2022-04-23 22:07:23 -0700

    changed test to iterate through sine wave

--- devices/servos/servo_test.py
+++ devices/servos/servo_test.py
@@ -18,7 +18,7 @@ import math
 import time
 import servo
 
-servo1 = servo.Servo(11,200)
+servo1 = servo.Servo(11,300)
 steps = 100
 sine = [round(270*math.sin(math.pi*n/steps)) for n in range(steps)]
 
@@ -26,13 +26,13 @@ try:
   while True:
     for n in sine:
         servo1.set_angle(n)
-        time.sleep(0.02)
-    for n in sine:
-        servo1.set_angle(n)
         time.sleep(0.01)
     for n in sine:
         servo1.set_angle(n)
-        time.sleep(0.005)
+        time.sleep(0.0066)
+    for n in sine:
+        servo1.set_angle(n)
+        time.sleep(0.0033)
 
 except KeyboardInterrupt:
     servo1.set_angle(0)
