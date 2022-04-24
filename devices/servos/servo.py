0eb04ac0beedeaea0f1c8e1d44fecfd51c9f331b
Bear Bissen <bearbissen@gmail.com>
2022-04-23 20:50:23 -0700

    added servo and servo test

---
+++ devices/servos/servo.py
@@ -0,0 +1,63 @@
+# ===============================================================================
+#
+# Name:       servo.py 
+#
+# Purpose:    Driver for servos when controlled by the raspberry pi
+#
+# Author:     Bear Bissen
+#
+# Created:    April 23, 2022
+# Last Rev:   
+# Edited by:  
+#
+# License: MIT Open License
+#
+# ===============================================================================
+
+import RPi.GPIO as GPIO
+from collections import namedtuple
+
+ServoAttributes = namedtuple('ServoAttributes',
+                            'min_angle max_angle\
+                            min_freq max_freq\
+                            min_pulse_width max_pulse_width')
+
+SERVO_ATTRIBUTES = {
+    'DS3225': ServoAttributes( 0, 270,              # Angle in Degrees
+                                50, 330,            # Frequency in Hz
+                                500e-6, 2500e-6)    # Pulse Width in Seconds
+}
+
+class Servo():
+
+    def __init__(self, pin, freq = 50, model = 'DS3225'):
+        self.pin = pin # GPIO pin on the Raspberry Pi refer to Board (#) not BCM GPIO#
+        self.freq = freq
+        self.model = model
+        self.attributes = SERVO_ATTRIBUTES[self.model]
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pin, self.freq)
+        self.min_duty_cycle = self.freq*self.attributes.min_pulse_width*100
+        self.max_duty_cycle = self.freq*self.attributes.max_pulse_width*100
+        self.duty_cycle = self.min_duty_cycle
+        self.angle = 0
+        self.pwm.start(self.min_duty_cycle)
+
+    def set_angle(self, angle):
+
+        if not isinstance(angle, (int,float)):
+            err_str = f'"{angle}" not a valid angle! (usage:int|float)'
+            raise RuntimeError(err_str) 
+
+        if angle < self.attributes.min_angle or angle > self.attributes.max_angle:
+            err_str = f"{angle} degrees out of range! ({self.attributes.min_angle} to {self.attributes.max_angle} degrees)"
+            raise RuntimeError(err_str)
+
+        self.angle = angle
+        self.duty_cycle = self.angle/self.attributes.max_angle*(self.max_duty_cycle-self.min_duty_cycle)+self.min_duty_cycle
+        self.pwm.ChangeDutyCycle(self.duty_cycle)
+
+    def stop_servo(self):
+        self.pwm.stop()
+        GPIO.cleanup()
---
+++ devices/servos/servo_test.py
@@ -0,0 +1,50 @@
+# ===============================================================================
+#
+# Name:       servo_test.py 
+#
+# Purpose:    Tests Servo Driver functionality
+#
+# Author:     Bear Bissen
+#
+# Created:    April 23, 2022
+# Last Rev:   
+# Edited by:  
+#
+# License: MIT Open License
+#
+# ===============================================================================
+
+import time
+import servo
+
+servo1 = servo.Servo(17)
+
+try:
+  while True:
+    servo1.set_angle(45)
+    time.sleep(0.5)
+    servo1.set_angle(90)
+    time.sleep(0.5)
+    servo1.set_angle(135)
+    time.sleep(0.5)
+    servo1.set_angle(180)
+    time.sleep(0.5)
+    servo1.set_angle(225)
+    time.sleep(0.5)
+    servo1.set_angle(270)
+    time.sleep(0.5)
+    servo1.set_angle(225)
+    time.sleep(0.5)
+    servo1.set_angle(180)
+    time.sleep(0.5)
+    servo1.set_angle(135)
+    time.sleep(0.5)
+    servo1.set_angle(90)
+    time.sleep(0.5)
+    servo1.set_angle(45)
+    time.sleep(0.5)
+    servo1.set_angle(0)
+    time.sleep(0.5)
+
+except KeyboardInterrupt:
+  servo1.stop_servo()
