5741185c040dece08ead8abbf6f0d91cccc46050
Bear Bissen <bearbissen@gmail.com>
2022-08-03 21:23:32 -0700

    replaced GPIO with pigpio

--- devices/servos/servo.py
+++ devices/servos/servo.py
@@ -53,19 +53,19 @@ class Servo(device.Device):
         self.pwm = pigpio.pi()
         self.pwm.set_mode(self.pin, pigpio.OUTPUT)
         self.pwm.set_PWM_frequency(self.pin, self.freq)
-        self.pwm.set_PWM_range(self.pin, 1000)
+        self.pwm.set_PWM_range(self.pin, 10000)
         self.min_duty_cycle = self.freq*self.attributes.min_pulse_width*100
         self.max_duty_cycle = self.freq*self.attributes.max_pulse_width*100
         self.duty_cycle = self.min_duty_cycle
         self.angle = 0
-        self.pwm.set_PWM_dutycycle(self.pin, self.min_duty_cycle*10)
+        self.pwm.set_PWM_dutycycle(self.pin, self.min_duty_cycle*100)
 
     def set_angle(self, angle):
         self.check_num(angle, "degrees", self.attributes.min_angle, self.attributes.max_angle)
 
         self.angle = angle
         self.duty_cycle = self.angle/self.attributes.max_angle*(self.max_duty_cycle-self.min_duty_cycle)+self.min_duty_cycle
-        self.pwm.set_PWM_dutycycle(self.pin, self.duty_cycle*10)
+        self.pwm.set_PWM_dutycycle(self.pin, self.duty_cycle*100)
         time.sleep(self.delay)
 
     def guide_to_angle(self, angle, speed):
